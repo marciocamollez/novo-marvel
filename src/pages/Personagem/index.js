6467eeb6049132f4a83c58c280bd5b8fb8773230
Marcio <marciocamollez@hotmail.com>
2022-04-27 11:59:53 -0300

    Instalacao axios e passando parametro por url do router dom

--- src/pages/Personagem/index.js
+++ src/pages/Personagem/index.js
@@ -1,11 +1,12 @@
+import { useParams } from 'react-router-dom';
 
 export const Personagem = () => {
 
- 
+    let params = useParams(); //Pegar id pela URL com o react router dom
 
     return(
         <div>
-            <h1>Personagem</h1>
+            <h1>Personagem nº{params.id}</h1>
             
         </div>
     )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useParams } from 'react-router-dom';
 import { Inicial } from '../pages/Inicial';
 import { Personagem } from '../pages/Personagem';
 
