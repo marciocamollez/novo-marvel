6fdf21f19e287a59c526b42dd6c90ce1d51aa2c2
Marcio <marciocamollez@hotmail.com>
2022-05-02 23:43:31 -0300

    Ajustes css mobile

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -4,12 +4,54 @@ import './style.css';
 
 const LikeButton = ({items}) => {
 
+    const [dark, setDark] = useState(
+        localStorage.getItem('dark-mode') === 'true'
+    );
 
-    const [like, setLike] = useState(100);
+    const personagensDb = localStorage['personagens'];
+    const personagens = personagensDb ? JSON.parse(personagensDb) : [];
+    
+    
+    const toggleDarkMode = () => {
+        setDark(!dark);
+        personagens.push(items);
+        localStorage['personagens'] = JSON.stringify(personagens);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('dark-mode', dark);
+        
+    }, [dark]);
+
+    
+
+    /*const taskMessage = 'Oi';
+    const taskName = 'Mundo';
+
+    const handleLike = () => {
+
+        localStorage.setItem("message", taskMessage);
+        localStorage.setItem("name", taskName);
+
+        var obj = {
+            "message": taskMessage,
+            "name": taskName
+        }
+
+        localStorage.setItem("task", JSON.stringify(obj));
+        var val = localStorage.getItem('obj');
+
+        console.log('retrievedValue', JSON.parse(val));
+
+    }*/
+
+
+    /*const [like, setLike] = useState(100);
     const [isLike, setIsLike] = useState();
 
     const personagensDb = localStorage['personagens'];
     const personagens = personagensDb ? JSON.parse(personagensDb) : [];
+
     
 
     const handleLike = () => {
@@ -33,13 +75,13 @@ const LikeButton = ({items}) => {
 
     }
 
-    /*useEffect(() => {
+    useEffect(() => {
         
     }, [isLike]);*/
 
     return(
         <div>
-            <button onClick={handleLike} className={"bt-like like-button " + (isLike ? "liked" : "")}>
+            <button onClick={toggleDarkMode} className={"bt-like like-button " + (dark ? "liked" : "")}>
                 {/*{like}*/}
             </button>
         </div>
