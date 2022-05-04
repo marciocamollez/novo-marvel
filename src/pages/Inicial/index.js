2a58e79a0a64c4f388ffde574e5b61781c881aaa
Marcio <marciocamollez@hotmail.com>
2022-05-04 11:27:54 -0300

    Troca de hash da conta marvel

--- src/pages/Inicial/index.js
+++ src/pages/Inicial/index.js
@@ -10,11 +10,12 @@ import { Ordenacao } from '../../components/Ordenacao';
 
 
 //hash = timestamp (1) + private key + public key convertido em md5
-//Hash da primeira conta criada. Deixar como backup
-//const hash = "21beb75ca82b20e52c8910f3e6599d79"
-//const apikey = "eb8c78fd1e6e98315a9d42fff3b5c040"
-const hash = "e8a129eee49b78fd4436bf9bb8102b3d";
-const apikey = "0b9047fa3f3f24bdf3933db0deb25d35";
+const hash = "21beb75ca82b20e52c8910f3e6599d79"
+const apikey = "eb8c78fd1e6e98315a9d42fff3b5c040"
+
+//Hash da segunda conta criada. Deixar como backup
+//const hash = "e8a129eee49b78fd4436bf9bb8102b3d";
+//const apikey = "0b9047fa3f3f24bdf3933db0deb25d35";
 
 export const Inicial = () => {
 
